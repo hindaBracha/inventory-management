09afe2ec83cb232a9d6bb569301c83a7e91d3e28
hinda bracha <hinda40311@gmail.com>
2024-12-29 00:55:44 +0200

    add ci/cd

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Product
+
+class ProductTestCase(TestCase):
+    def test_product_creation(self):
+        product = Product.objects.create(name="Test Product", price=10.0)
+        self.assertEqual(product.name, "Test Product")
+        self.assertEqual(product.price, 10.0)
